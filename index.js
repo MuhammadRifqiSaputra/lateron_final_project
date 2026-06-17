1f329f8a6e29e5b6bbbafbb53840b02ce721f95a
MuhammadRifqiSaputra <muhammadrifqi.23002@mhs.unesa.ac.id>
2026-06-17 14:57:25 +0700

    serve react build from express

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
 const app = express();
-const cors = require('cors');
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/users', require('./routes/users'));
-app.use('/test-targets', require('./routes/testTargets'));
-app.use('/roadmaps', require('./routes/roadmaps'));
-app.use('/daily-schedules', require('./routes/dailySchedules'));
-app.use('/checklists', require('./routes/checklists'));
-app.use('/reminders', require('./routes/reminders'));
-app.use('/study-streaks', require('./routes/studyStreaks'));
-app.use('/quiz', require('./routes/quizzes'));
+// API Routes
+app.use("/users", require("./routes/users"));
+app.use("/test-targets", require("./routes/testTargets"));
+app.use("/roadmaps", require("./routes/roadmaps"));
+app.use("/daily-schedules", require("./routes/dailySchedules"));
+app.use("/checklists", require("./routes/checklists"));
+app.use("/reminders", require("./routes/reminders"));
+app.use("/study-streaks", require("./routes/studyStreaks"));
+app.use("/quiz", require("./routes/quizzes"));
+
+// Serve React Build
+app.use(express.static(path.join(__dirname, "dist")));
+
+// React Router Fallback
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 
 const PORT = process.env.PORT || 3000;
 
